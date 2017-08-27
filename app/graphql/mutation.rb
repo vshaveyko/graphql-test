26dc00eab04d602671d59c436a929bfdc5d69bce
vs <vshaveyko@gmail.com>
2017-08-27 21:00:16 +0300

    Fix indent

--- app/graphql/mutation.rb
+++ app/graphql/mutation.rb
@@ -6,6 +6,6 @@ Mutation = GraphQL::ObjectType.define do
   user_input_types = [User::InputType]
   field :CreateUser  , User::Type      , function: Function.Create(model: User, input_types: user_input_types)
   field :UpdateUser  , User::Type      , function: Function.Update(model: User, input_types: user_input_types)
-  field :DestroyUser , types.Boolean , function: Function.Destroy(model: User)
+  field :DestroyUser , types.Boolean   , function: Function.Destroy(model: User)
 
 end
